e8f1925d72375973346c0d8e0561ab7973286ead
Jean-François Van Ass <jfvanass@gmail.com>
2021-02-12 09:11:49 +0100

    #313510 add missing lang

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -1039,6 +1039,7 @@ $string['filters'] = 'Filters';
 $string['icqnumber'] = 'ICQ number';
 $string['icon'] = 'Icon';
 $string['iconsize'] = 'Icon Size';
+$string['iconsize_help'] = 'This setting determines the size in pixels of the course module icon.';
 $string['idnumber'] = 'ID number';
 $string['idnumbercourse'] = 'Course ID number';
 $string['idnumbercourse_help'] = 'The ID number of a course is only used when matching the course against external systems and is not displayed anywhere on the site. If the course has an official code name it may be entered, otherwise the field can be left blank.';
